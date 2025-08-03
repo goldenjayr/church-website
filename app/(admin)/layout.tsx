065d17346db7f72e538f79bf728488e17eddd833
Jay-R Joseph Gabunada <jayr@23point5.com>
2025-08-03 21:57:06 +0800

    added metadata in admin

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -9,9 +9,20 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Divine Jesus Church",
   description: "A welcoming community of faith, hope, and love",
+  openGraph: {
+    title: "Divine Jesus Church",
+    description: "A welcoming community of faith, hope, and love",
+    images: [
+      {
+        url: "https://cdn.jsdelivr.net/gh/goldenjayr/divinejesus-files/hero.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Divine Jesus Church",
+      },
+    ],
+  },
     generator: 'v0.dev'
 }
-
 export default function RootLayout({
   children,
 }: {
