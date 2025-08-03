26b364ceab13c378bed6c08c04f250f3dc08aaa9
Jay-R Joseph Gabunada <jayr@23point5.com>
2025-08-03 21:25:43 +0800

    updated blog loading

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,22 @@
+import type { BlogPost, User as UserType, BlogCategory } from "@prisma/client"
+
 export interface IVerse {
   text: string
   reference: string
+}
+
+export type BlogPostWithAuthor = BlogPost & {
+  author: UserType
+  member?: {
+    id: string
+    firstName: string
+    lastName: string
+    imageUrl?: string | null
+    position?: {
+      id: string
+      name: string
+      color: string
+    } | null
+  } | null
+  category: BlogCategory | null
 }
