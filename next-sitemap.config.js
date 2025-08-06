a6deb2301ea26b97acb8079c76a7e0376fb6af3b
Jay-R Joseph Gabunada <jayr@23point5.com>
2025-08-06 17:53:22 +0800

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -10,5 +10,18 @@ module.exports = {
         disallow: ['/admin'],
       },
     ],
-  }
+  },
+  // Default transformation function
+  transform: async (config, path) => {
+    if (path.endsWith('.png')) {
+      return null
+    }
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
 }
